35eb46e80920ab2cddac8f3f0b61846dfbca2d36
Tiffany Ferderer <tferderer@mail.greenriver.edu>
2021-01-15 15:17:04 -0500

    Index page created

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Tiffany Ferderer
+ * 1/15/2021
+ * http://tferderer.greenriverdev.com/SDEV328/pp2/index.php
+ * PairProgram 2
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<?php
+echo "<h1>PHP Array Practice</h1>";
+?>
+
+</body>
+</html>
